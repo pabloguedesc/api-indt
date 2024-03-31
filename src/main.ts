e39f77b13e5daa8a8248bb207b919c5a8e6f58be
Pablo Costa <pablo.costa@grupoicts.com.br>
2024-03-31 15:08:38 -0400

    ajustando habilitador de cors

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,10 @@ dotenv.config();
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(cors());
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api/v1');
   await app.listen(process.env.PORT || 3000);
